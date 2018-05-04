f7cd810dd1773623b439b516a4219007d2a06403
t-sin <shinichi.tanaka45@gmail.com>
2018-05-05 02:18:05 +0900

    WIP: adding and showing user

--- controllers/users.lisp
+++ controllers/users.lisp
@@ -21,5 +21,20 @@
   (declare (ignore params))
   (render nil :template "users/add.html.dj"))
 
-(defun add- (params)
-  "aaaaaaaaaaaaaaaa")
+(defun add* (params)
+  (let ((github-name (cdr (assoc "github-username" params :test #'string=)))
+        (slack-name (cdr (assoc "slack-username" params :test #'string=)))
+        (user (make-instance 'users)))
+
+    (multiple-value-bind (id name)
+        (api/user github-name)
+      (setf (users-github-id user) id)
+      (setf (users-github-name user) name))
+
+    ;;; TODO: slack API
+    (setf (users-slack-id user) "hoge-id")
+    (setf (users-slack-name user) "hoge-user")
+
+    (mito:save-dao user)
+    (render (list :users (mito:select-dao 'users))
+            :template "users/lists.html.dj")))
