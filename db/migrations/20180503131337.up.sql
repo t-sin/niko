04d40fbfcecef627cfd9bd105d9fa853be97aca4
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 00:16:53 +0900

    Move and remove, about DB

--- db/migrations/20180503131337.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "users" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "github_id" STRING NOT NULL,
-    "slack_id" STRING NOT NULL,
-    "created_at" TIMESTAMP,
-    "updated_at" TIMESTAMP
-);
