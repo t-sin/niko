1d6415f31ce2832814315c993333da1b0da2ac33
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 02:40:54 +0900

    Introduce some macros for define endpoint

--- app.lisp
+++ app.lisp
@@ -7,7 +7,10 @@
   (:import-from #:niko/util
                 #:project-root)
   (:export #:*napp*
-           #:*app*))
+           #:*app*
+           #:assoc*
+           #:defroute
+           #:defapi))
 (in-package #:niko/app)
 
 (defparameter *napp* (make-instance '<app>))
@@ -16,3 +19,38 @@
   (lack:builder
    (:static :path "/public/" :root (project-root #P"public/"))
    *napp*))
+
+(defun assoc* (str alist)
+  (cdr (assoc str alist :test #'string=)))
+
+(defun to-json* (obj)
+  (let ((jojo:*null-value* :null)
+        (jojo:*false-value* :false)
+        (jojo:*empty-array-value* :[])
+        (jojo:*empty-object-value* :{}))
+    (to-json obj)))
+
+(defmacro append-header (name value)
+  `(setf (lack.response:response-headers ningle:*response*)
+        (append (lack.response:response-headers ningle:*response*)
+                (list ,name ,value))))
+
+(defmacro defroute ((path method) &body body)
+  `(setf (ningle:route niko/app:*napp* ,path :method ,method)
+         (lambda (params)
+           (declare (ignorable params))
+           (symbol-macrolet ((status-code (lack.response:response-status ningle:*response*)))
+             ,@body))))
+
+(defmacro with-json (&body body)
+  `(let ((result (progn ,@body)))
+     (append-header :content-type "application/json")
+     (append-header :access-control-allow-origin "*")
+     (append-header :access-control-allow-headres "Content-type, Authorization")
+     (let ((result (progn ,@body)))
+       (to-json* result))))
+
+(defmacro defapi ((path method) &body body)
+  `(progn
+     (defroute (,path :OPTIONS) (with-json "OK"))
+     (defroute (,path ,method) (with-json ,@body))))
