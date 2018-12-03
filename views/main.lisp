211fc4ab73ea22cc52327a82ed8ec824d5ad92c8
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 03:08:04 +0900

    Welcome back root page!

--- views/main.lisp
+++ views/main.lisp
@@ -2,10 +2,19 @@
   (:use #:cl #:lsx)
   (:import-from #:niko/app
                 #:defroute)
+  (:import-from #:niko/util
+                #:project-root)
   (:export #:view-template))
-(in-package #:niko/views)
+(in-package #:niko/views/main)
+
+(defun get-view (name)
+  (let ((name (format nil "~a.lsx" (string-downcase (symbol-name name)))))
+    (read-lsx-file (merge-pathnames name (project-root #P"views/")))))
+
+(defparameter *page-template* (get-view :template))
 
 (defroute ("/" :GET)
-  (let ((template (read-lsx-file #P"views/template.lsx")))
-    (render-object (funcall template
-                            :title "Niko - Not-a-cat Slack bot") nil)))
+  (let ((lsx:*auto-escape* nil))
+    (render-object (funcall *page-template*
+                            :title "Niko - Not-a-cat Slack bot"
+                            :body (get-view :root)) nil)))
