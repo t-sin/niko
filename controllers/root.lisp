c11345ea8321948c5718b57b53aed4e022984e30
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 16:15:22 +0900

    Remove controller because of migration completed

--- controllers/root.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:niko/controllers/root
-  (:use #:cl
-        #:utopian)
-  (:export #:index))
-(in-package #:niko/controllers/root)
-
-(defun index (params)
-  (declare (ignore params))
-  (render nil :template :index))
