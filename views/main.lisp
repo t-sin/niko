843c3df98f46a676eb49c1437d3fe105e41078af
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 11:11:36 +0900

    Restore /user/list and /user/add without action, as LSX

--- views/main.lisp
+++ views/main.lisp
@@ -18,3 +18,19 @@
     (render-object (funcall *page-template*
                             :title "Niko - Not-a-cat Slack bot"
                             :body (get-view :root)) nil)))
+
+(defroute ("/user/add" :GET)
+  (let ((lsx:*auto-escape* nil))
+    (render-object (funcall *page-template*
+                            :title "Niko - Register an user"
+                            :body (get-view :user-add)) nil)))
+
+(defparameter *users*
+  '((:id 1 :github-id 1234 :github-name "t-sin" :slack-id 12 :slack-name "t-sin")
+    (:id 2 :github-id 111 :github-name "test" :slack-id 345 :slack-name "hoge")))
+(defroute ("/user/list" :GET)
+  (let* ((lsx:*auto-escape* nil)
+         (user-list (get-view :user-list))
+         (rendered-body (render-object (funcall user-list :users *users*) nil)))
+    (render-object (funcall *page-template*
+                            :title "Niko - Register an user" :body rendered-body) nil)))
