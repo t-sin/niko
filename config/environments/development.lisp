5cdf065a1ea3f278e755d091b2af16f776a0da07
t-sin <shinichi.tanaka45@gmail.com>
2018-12-04 17:55:18 +0900

    Remove `config` because of routing migration completed

--- config/environments/development.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:niko/config/environments/development
-  (:use #:cl
-        #:utopian))
-(in-package #:niko/config/environments/development)
-
-`(:databases
-  ((:maindb . (:sqlite3
-               :database-name ,(project-path #P"db/development.db"))))
-  :error-log ,(project-path #P"log/error.log"))
